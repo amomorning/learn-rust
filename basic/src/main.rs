b4c23f9052becee6d2b04a3efdf96f039a51a945
Yichen Mo <amomorning@gmail.com>
2026-04-01 16:01:46 +0800

    learn cell

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,17 +1,21 @@
+use std::cell::Cell;
+
 fn add(x: i32, y: i32) -> i32 {
     x + y
 }
-
 
 fn main() {
     let a = 10;
     let b: i32 = 20;
 
-    let (mut c, d) = (30_i32, 40_i32);
-    let e = add(add(a, b), add(c, d));
-    c = 50;
-    println!("The value of c is: {}", c);
+    let (c, d) = (Cell::new(30_i32), 40_i32);  // c 用 Cell 包装
 
-    println!("The value of e is: {}", e);
+    let e = || add(add(a, b), add(c.get(), d));  // 通过 get() 读取
+
+    println!("e before: {}", e());  // 100
+
+    c.set(50);  // 通过 set() 修改
+    println!("c is: {}", c.get());
+
+    println!("e after: {}", e());  // 120
 }
-
